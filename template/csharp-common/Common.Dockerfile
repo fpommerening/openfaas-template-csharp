f538baab449d1c3a608563c22cbb846bbcef51c2
Frank Pommerening <frank@pommerening-online.de>
2018-02-20 19:36:11 +0100

    start common - template

---
+++ template/csharp-common/Common.Dockerfile
@@ -0,0 +1,3 @@
+FROM microsoft/dotnet:2.0-sdk
+
+COPY ./common/  /root/src/
---
+++ template/csharp-common/Program.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using Function;
+using OpenFaaS.Dotnet;
+
+namespace root
+{
+    class Program
+    {
+        private static string getStdin() {
+            StringBuilder buffer = new StringBuilder();
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                buffer.AppendLine(s);
+            }
+            return buffer.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string buffer = getStdin();
+            var context = new DefaultFunctionContext();
+            FunctionHandler f = new FunctionHandler(context);
+            f.Handle(buffer);
+        }
+    }
+}
