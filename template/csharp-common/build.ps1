0b1a2978675e2c4cac7cae60fc4d60bc7d785fa7
Frank Pommerening <frank@pommerening-online.de>
2018-02-26 19:53:31 +0100

    Add powershell scripts

--- template/csharp-common/build.ps1
+++ template/csharp-common/build.ps1
@@ -1,1 +1,12 @@
-docker build -t common 
+
+$cli_path = "C:\OpenFaaS\faas-cli.exe"
+
+& docker build -t csharp-common -f ./template/csharp-common/Common.Dockerfile .
+
+$stack_file = "stack.yml"
+
+if(Test-Path *.yml)
+{
+    $stack_file = (Get-Childitem *.yml).Name
+}
+& $cli_path build -f $stack_file
--- template/csharp-common/new.ps1
+++ template/csharp-common/new.ps1
@@ -1,2 +1,24 @@
+param([string]$ProjectName='')
+
 $cli_path = "C:\OpenFaaS\faas-cli.exe"
+
+if(Test-Path *.yml)
+{
+    & $cli_path 'new' $ProjectName --lang csharp-common --append ((Get-Childitem *.yml).Name)
+}
+else
+{
+    & $cli_path 'new' $ProjectName --lang csharp-common -f stack.yml
+}
+
+if(-not(Test-Path *.sln))
+{
+    & dotnet new sln 
+}
+
+& dotnet new classlib -n $ProjectName -lang 'c#'
+& dotnet sln add ./$ProjectName/$ProjectName.csproj
+Remove-Item ./$ProjectName/Class1.cs
+
+& dotnet add ./$ProjectName/$ProjectName.csproj package OpenFaaS.DotNet
 
