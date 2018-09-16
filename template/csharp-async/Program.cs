66dd427d9c5d7f328909392db78b1ccf23ba979e
Frank Pommerening <frank.pommerening@axp-consulting.de>
2018-09-16 13:13:28 +0200

    Async template

---
+++ template/csharp-async/Program.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using Function;
+
+namespace root
+{
+    class Program
+    {
+        private static string getStdin() {
+            StringBuilder buffer = new StringBuilder();
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                buffer.AppendLine(s);
+            }
+            return buffer.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().Result();
+        }
+
+        static async Task MainAsync()
+        {
+            string buffer = getStdin();
+            FunctionHandler f = new FunctionHandler();
+
+            string responseValue = await f.Handle(buffer);
+
+            if(responseValue != null) {
+                Console.Write(responseValue);
+            }
+        }
+    }
+}
---
+++ template/csharp-async/function/FunctionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+          public Task<string> Handle(string input) 
+          {
+            return Task.FromResult(string.Format("Hi there - your input was: {0}\n", input));
+        }
+    }
+}
